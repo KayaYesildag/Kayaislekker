477c659f7e836bf990f05ec0128f56c4cd7da224
kayayesildag@gmail.com <kayayesildag@gmail.com>
2012-05-07 11:38:06 +0200

    haaaa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kayaislekker::Application.config.secret_token = '26fa19919b38036cfba9eb198c329c12076fcc6e30fa2d4af8276f3dfc3d5217efa570333c289628a831f3b5070090c812b5ca4d1644a7c2ee345dc28f68b33f'
